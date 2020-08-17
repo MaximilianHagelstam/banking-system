4cb797912e723f81a3b2ebfeeb286e8ffe5621a5
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2020-08-17 09:42:03 +0300

    finished

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Banking system
+When run the app you will have to create an account.
+You can also create multiple accounts.
+After creating an account you are greeted with a five option menu.
+You can show all accounts, search for an account, withdraw, deposit or exit the app.
+
 ### Purpose
 The purpose of this project is to practise the four fundamental concepts of object oriented programming (OOP) in java:
 - Inheritance
 - Polymorphism
 - Abstraction
 - Encapsulation
+
+###What I Learned
+I didn't get to practise inheritance or polymorphism, but I still practised the following java fundamentals:
+- Classes
+- Methods
+- User input
+- Do-while and For loops
+- Arrays
+- Switch and If-else statements
---
+++ src/App.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] arg) {
+        Scanner sc = new Scanner(System.in);
+
+        //Create account
+        System.out.print("How Many Accounts Do You Want To Create: ");
+        int n = sc.nextInt();
+        Bank[] C = new Bank[n];
+        for (int i = 0; i < C.length; i++) {
+            C[i] = new Bank();
+            C[i].openAccount();
+        }
+
+        //Loops until user exits (presses 5)
+        int choice;
+        do {
+            System.out.println("Show Menu\n 1. Show All Accounts\n 2. Search For Account\n 3. Deposit\n 4. Withdraw\n 5.Exit");
+            System.out.println("Your Choice: "); choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    for (Bank bank : C) {
+                        bank.showAccount();
+                    }
+                    break;
+
+                case 2:
+                    System.out.print("Account Number You Want To Search For: ");
+                    String accountNum = sc.next();
+
+                    boolean accountFound = false;
+                    for (Bank bank : C) {
+                        accountFound = bank.search(accountNum);
+                        if (accountFound) {
+                            break;
+                        }
+                    }
+
+                    if (!accountFound) {
+                        System.out.println("Didn't find account");
+                    }
+                    break;
+
+                case 3:
+                    System.out.print("Enter Account Number To Deposit To: ");
+                    accountNum = sc.next();
+                    accountFound = false;
+
+                    for (Bank bank : C) {
+                        accountFound = bank.search(accountNum);
+                        if (accountFound) {
+                            bank.deposit();
+                            break;
+                        }
+                    }
+
+                    if (!accountFound) {
+                        System.out.println("Didn't find account");
+                    }
+                    break;
+
+                case 4:
+                    System.out.print("Enter Account Number To Withdraw From: ");
+                    accountNum = sc.next();
+                    accountFound = false;
+
+                    for (Bank bank : C) {
+                        accountFound = bank.search(accountNum);
+                        if (accountFound) {
+                            bank.withdrawal();
+                            break;
+                        }
+                    }
+
+                    if (!accountFound) {
+                        System.out.println("Didn't find account");
+                    }
+                    break;
+
+                case 5:
+                    System.out.println("App Closed");
+                    break;
+            }
+        }
+        while (choice != 5);
+    }
+}
